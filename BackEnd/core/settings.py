a4b789f0900062ec5de391feca7fa5b56214ac87
TrangNPDCE180628 <trangnpdce180628@fpt.edu.vn>
2025-06-27 15:36:47 +0700

    Update final TrangNPD

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
--- FrontEnd/src/constants/products.ts
+++ FrontEnd/src/constants/products.ts
@@ -1,145 +1,594 @@
 const products = [
   {
-    id: 1,
-    name: "Gradient Graphic T-shirt",
-    image: "/images/products/Gradient-Graphic-T-shirt.webp",
-    price: 120,
-    color: ["pink", "white", "black"],
-    size: ["Small", "Medium", "Large", "X-Large"],
-    rating: 4.5,
+    "id": 1,
+    "name": "Gradient Graphic T-shirt",
+    "image": "http://127.0.0.1:8000/media/products/Gradient-Graphic-T-shirt.webp",
+    "price": "120.00",
+    "old_price": null,
+    "discount": null,
+    "rating": 4.5,
+    "colors": [
+      {
+        "id": 8,
+        "name": "pink"
+      },
+      {
+        "id": 9,
+        "name": "white"
+      },
+      {
+        "id": 10,
+        "name": "black"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      }
+    ],
+    "category": {
+      "id": 1,
+      "name": "T-shirts"
+    }
   },
   {
-    id: 2,
-    name: "Polo with Tipping Details",
-    image: "/images/products/Polo-with-Tipping-Details.webp",
-    price: 240,
-    oldPrice: 260,
-    discount: "20%",
-    color: ["red", "yellow", "orange"],
-    size: ["XX-Small", "X-Small", "Small", "Medium", "Large",],
-    rating: 3.5,
+    "id": 2,
+    "name": "Polo with Tipping Details",
+    "image": "http://127.0.0.1:8000/media/products/Polo-with-Tipping-Details.webp",
+    "price": "240.00",
+    "old_price": "260.00",
+    "discount": "20",
+    "rating": 3.5,
+    "colors": [
+      {
+        "id": 2,
+        "name": "red"
+      },
+      {
+        "id": 3,
+        "name": "yellow"
+      },
+      {
+        "id": 4,
+        "name": "orange"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 1,
+        "name": "XX-Small"
+      },
+      {
+        "id": 2,
+        "name": "X-Small"
+      },
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      }
+    ],
+    "category": {
+      "id": 2,
+      "name": "Shorts"
+    }
   },
   {
-    id: 3,
-    name: "Black Striped T-shirt",
-    image: "/images/products/Black-Striped-T-shirt.webp",
-    price: 180,
-    color: ["red", "yellow", "orange"],
-    size: ["Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large", "4X-Large"],
-    rating: 4.5,
+    "id": 3,
+    "name": "Black Striped T-shirt",
+    "image": "http://127.0.0.1:8000/media/products/Black-Striped-T-shirt.webp",
+    "price": "180.00",
+    "old_price": null,
+    "discount": null,
+    "rating": 4.5,
+    "colors": [
+      {
+        "id": 2,
+        "name": "red"
+      },
+      {
+        "id": 3,
+        "name": "yellow"
+      },
+      {
+        "id": 4,
+        "name": "orange"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      },
+      {
+        "id": 7,
+        "name": "XX-Large"
+      },
+      {
+        "id": 8,
+        "name": "3X-Large"
+      },
+      {
+        "id": 9,
+        "name": "4X-Large"
+      }
+    ],
+    "category": {
+      "id": 1,
+      "name": "T-shirts"
+    }
   },
   {
-    id: 4,
-    name: "SKINNY FIT JEANS",
-    image: "/images/products/SKINNY-FIT-JEANS.webp",
-    price: 130,
-    oldPrice: 160,
-    discount: "30%",
-    color: ["cyan", "blue", "purple"],
-    size: [ "X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large", "4X-Large"],
-    rating: 4,
+    "id": 7,
+    "name": "VERTICAL STRIPED SHIRT",
+    "image": "http://127.0.0.1:8000/media/products/VERTICAL-STRIPED-SHIRT.webp",
+    "price": "180.00",
+    "old_price": null,
+    "discount": null,
+    "rating": 4.5,
+    "colors": [
+      {
+        "id": 1,
+        "name": "green"
+      },
+      {
+        "id": 2,
+        "name": "red"
+      },
+      {
+        "id": 7,
+        "name": "purple"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 2,
+        "name": "X-Small"
+      },
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      },
+      {
+        "id": 7,
+        "name": "XX-Large"
+      },
+      {
+        "id": 8,
+        "name": "3X-Large"
+      },
+      {
+        "id": 9,
+        "name": "4X-Large"
+      }
+    ],
+    "category": {
+      "id": 1,
+      "name": "T-shirts"
+    }
   },
   {
-    id: 5,
-    name: "CHECKERED SHIRT",
-    image: "/images/products/CHECKERED-SHIRT.webp",
-    price: 120,
-    color: ["cyan", "green", "purple"],
-    size: ["XX-Small", "X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large"],
-    rating: 4.5,
+    "id": 8,
+    "name": "COURAGE GRAPHIC T-SHIRT",
+    "image": "http://127.0.0.1:8000/media/products/COURAGE-GRAPHIC-T-SHIRT.webp",
+    "price": "130.00",
+    "old_price": "160.00",
+    "discount": "30",
+    "rating": 4,
+    "colors": [
+      {
+        "id": 6,
+        "name": "blue"
+      },
+      {
+        "id": 7,
+        "name": "purple"
+      },
+      {
+        "id": 8,
+        "name": "pink"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 1,
+        "name": "XX-Small"
+      },
+      {
+        "id": 2,
+        "name": "X-Small"
+      },
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      },
+      {
+        "id": 7,
+        "name": "XX-Large"
+      },
+      {
+        "id": 8,
+        "name": "3X-Large"
+      },
+      {
+        "id": 9,
+        "name": "4X-Large"
+      }
+    ],
+    "category": {
+      "id": 1,
+      "name": "T-shirts"
+    }
   },
   {
-    id: 6,
-    name: "SLEEVE STRIPED T-SHIRT",
-    image: "/images/products/SLEEVE-STRIPED-T-SHIRT.webp",
-    price: 240,
-    oldPrice: 260,
-    discount: "20%",
-    color: ["black", "green", "purple"],
-    size: ["XX-Small", "X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large", "4X-Large"],
-    rating: 3.5,
+    "id": 9,
+    "name": "LOOSE FIT BERMUDA SHORTS",
+    "image": "http://127.0.0.1:8000/media/products/LOOSE-FIT-BERMUDA-SHORTS.webp",
+    "price": "120.00",
+    "old_price": null,
+    "discount": null,
+    "rating": 4.5,
+    "colors": [
+      {
+        "id": 2,
+        "name": "red"
+      },
+      {
+        "id": 7,
+        "name": "purple"
+      },
+      {
+        "id": 8,
+        "name": "pink"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      }
+    ],
+    "category": {
+      "id": 2,
+      "name": "Shorts"
+    }
   },
   {
-    id: 7,
-    name: "VERTICAL STRIPED SHIRT",
-    image: "/images/products/VERTICAL-STRIPED-SHIRT.webp",
-    price: 180,
-
-    color: ["green", "red", "purple"],
-    size: ["X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large", "4X-Large"],
-    rating: 4.5,
+    "id": 10,
+    "name": "Polo with Contrast Trims",
+    "image": "http://127.0.0.1:8000/media/products/Polo-with-Contrast-Trims.webp",
+    "price": "240.00",
+    "old_price": "260.00",
+    "discount": "20",
+    "rating": 3.5,
+    "colors": [
+      {
+        "id": 6,
+        "name": "blue"
+      },
+      {
+        "id": 7,
+        "name": "purple"
+      },
+      {
+        "id": 8,
+        "name": "pink"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 2,
+        "name": "X-Small"
+      },
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      },
+      {
+        "id": 7,
+        "name": "XX-Large"
+      }
+    ],
+    "category": {
+      "id": 4,
+      "name": "Hoodie"
+    }
   },
   {
-    id: 8,
-    name: "COURAGE GRAPHIC T-SHIRT",
-    image: "/images/products/COURAGE-GRAPHIC-T-SHIRT.webp",
-    price: 130,
-    oldPrice: 160,
-    discount: "30%",
-    color: ["blue", "purple", "pink"],
-    size: ["XX-Small", "X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large", "4X-Large"],
-    rating: 4,
+    "id": 11,
+    "name": "One Life Graphic T-shirt",
+    "image": "http://127.0.0.1:8000/media/products/One-Life-Graphic-T-shirt.webp",
+    "price": "180.00",
+    "old_price": null,
+    "discount": null,
+    "rating": 4.5,
+    "colors": [
+      {
+        "id": 2,
+        "name": "red"
+      },
+      {
+        "id": 3,
+        "name": "yellow"
+      },
+      {
+        "id": 6,
+        "name": "blue"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 2,
+        "name": "X-Small"
+      },
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      },
+      {
+        "id": 7,
+        "name": "XX-Large"
+      },
+      {
+        "id": 8,
+        "name": "3X-Large"
+      }
+    ],
+    "category": {
+      "id": 1,
+      "name": "T-shirts"
+    }
   },
   {
-    id: 9,
-    name: "LOOSE FIT BERMUDA SHORTS",
-    image: "/images/products/LOOSE-FIT-BERMUDA-SHORTS.webp",
-    price: 120,
-    color: ["red", "purple", "pink"],
-    size: ["Small", "Medium", "Large", "X-Large"],
-    rating: 4.5,
+    "id": 12,
+    "name": "Sleeve Striped T-shirt",
+    "image": "http://127.0.0.1:8000/media/products/SLEEVE-STRIPED-T-SHIRT_q0iQEBW.webp",
+    "price": "130.00",
+    "old_price": "160.00",
+    "discount": "30",
+    "rating": 4,
+    "colors": [
+      {
+        "id": 2,
+        "name": "red"
+      },
+      {
+        "id": 3,
+        "name": "yellow"
+      },
+      {
+        "id": 4,
+        "name": "orange"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      },
+      {
+        "id": 7,
+        "name": "XX-Large"
+      },
+      {
+        "id": 8,
+        "name": "3X-Large"
+      }
+    ],
+    "category": {
+      "id": 1,
+      "name": "T-shirts"
+    }
   },
   {
-    id: 10,
-    name: "Polo with Contrast Trims",
-    image: "/images/products/Polo-with-Contrast-Trims.webp",
-    price: 240,
-    oldPrice: 260,
-    discount: "20%",
-
-    color: ["blue", "purple", "cyan"],
-    size: ["X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large"],
-    rating: 3.5,
+    "id": 13,
+    "name": "T-shirt with Tape Details",
+    "image": "http://127.0.0.1:8000/media/products/T-SHIRT-WITH-TAPE-DETAILS.webp",
+    "price": "120.00",
+    "old_price": null,
+    "discount": null,
+    "rating": 4.5,
+    "colors": [
+      {
+        "id": 3,
+        "name": "yellow"
+      },
+      {
+        "id": 4,
+        "name": "orange"
+      },
+      {
+        "id": 10,
+        "name": "black"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 1,
+        "name": "XX-Small"
+      },
+      {
+        "id": 2,
+        "name": "X-Small"
+      },
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      },
+      {
+        "id": 7,
+        "name": "XX-Large"
+      }
+    ],
+    "category": {
+      "id": 1,
+      "name": "T-shirts"
+    }
   },
   {
-    id: 11,
-    name: "One Life Graphic T-shirt",
-    image: "/images/products/One-Life-Graphic-T-shirt.webp",
-    price: "180",
-    color: ["blue", "red", "yellow"],
-    size: ["X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large"],
-    rating: 4.5,
-  },
-  {
-    id: 12,
-    name: "Sleeve Striped T-shirt",
-    image: "/images/products/SLEEVE-STRIPED-T-SHIRT.webp",
-    price: "130",
-    oldPrice: "160",
-    discount: "30%",
-    color: ["orange", "red", "yellow"],
-    size: ["Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large"],
-    rating: 4,
-  },
-  {
-    id: 13,
-    name: "T-shirt with Tape Details",
-    image: "/images/products/T-SHIRT-WITH-TAPE-DETAILS.webp",
-    price: "120",
-    color: ["orange", "black", "yellow"],
-    size: ["XX-Small", "X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large"],
-    rating: 4.5,
-  },
-  {
-    id: 14,
-    name: "Skinny Fit Jeans",
-    image: "/images/products/SKINNY-FIT-JEANS.webp",
-    price: "240",
-    oldPrice: "260",
-    discount: "20%",
-    colors: ["green", "white", "black"],
-    size: ["Small", "Medium", "Large", "X-Large", "XX-Large"],
-    rating: 3.5,
+    "id": 14,
+    "name": "Skinny Fit Jeans",
+    "image": "http://127.0.0.1:8000/media/products/SKINNY-FIT-JEANS_bUYs4xl.webp",
+    "price": "240.00",
+    "old_price": "260.00",
+    "discount": "20",
+    "rating": 3.5,
+    "colors": [
+      {
+        "id": 1,
+        "name": "green"
+      },
+      {
+        "id": 9,
+        "name": "white"
+      },
+      {
+        "id": 10,
+        "name": "black"
+      }
+    ],
+    "sizes": [
+      {
+        "id": 3,
+        "name": "Small"
+      },
+      {
+        "id": 4,
+        "name": "Medium"
+      },
+      {
+        "id": 5,
+        "name": "Large"
+      },
+      {
+        "id": 6,
+        "name": "X-Large"
+      },
+      {
+        "id": 7,
+        "name": "XX-Large"
+      }
+    ],
+    "category": {
+      "id": 5,
+      "name": "Jeans"
+    }
   },
   {
     id: 15,
@@ -198,7 +647,7 @@ const products = [
     oldPrice: "160",
     discount: "30%",
     colors: ["red", "yellow", "orange"],
-    size: [ "X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large", "4X-Large"],
+    size: ["X-Small", "Small", "Medium", "Large", "X-Large", "XX-Large", "3X-Large", "4X-Large"],
     rating: 4,
   },
   {
