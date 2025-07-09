95b84a81e92d68387b7a4598d4d9eab6ce5b2d2d
TrangNPDCE180628 <trangnpdce180628@fpt.edu.vn>
2025-07-09 17:49:36 +0700

    Fixed: type Product

--- FrontEnd/src/types/product.types.ts
+++ FrontEnd/src/types/product.types.ts
@@ -7,11 +7,13 @@ export type Product = {
   id: number;
   name: string;
   image: string;
-  price: number;
-  old_price?: number | null;
+  price: string | number; 
+  old_price?: string | number | null;
+  oldPrice?: string | number | null; 
   discount?: string | null;
   rating: number;
-  colors: { id: number; name: string }[];
-  sizes: { id: number; name: string }[];
-  category: { id: number; name: string };
+  colors: ({ id?: number; name: string } | string)[];
+  sizes: ({ id?: number; name: string } | string)[];
+  category?: { id: number; name: string }; 
 };
+
